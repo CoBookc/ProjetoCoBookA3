4e71df556575c83dd2960755ea265e6d7a4ace6f
Cobookc <cobookc@gmail.com>
2023-10-22 14:19:09 -0300

    Mudando atributos

---
+++ BibliotecaCoBook/src/dao/ConnectionMVC.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ConnectionMVC {
+    
+    public Connection getConnection() {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cobook?useSSL=false", "root", "1234");
+        } catch (SQLException e) {
+        e.printStackTrace();
+        }
+ 
+        
+        
+        return conn;
+    }
+    
+}
