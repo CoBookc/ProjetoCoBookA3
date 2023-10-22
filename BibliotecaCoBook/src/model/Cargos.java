216d6252fd6dc3b251a2411bed4bf53777c52ab3
Cobookc <cobookc@gmail.com>
2023-10-22 13:16:22 -0300

    Criando classes no model

---
+++ BibliotecaCoBook/src/model/Cargos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Cargos {
+    private Integer codCargos;
+    private String tipoFuncao;
+}
---
+++ BibliotecaCoBook/src/model/Usuarios.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Usuarios {
+    private Integer codUsuarios;
+    private Integer idade;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String sexo;
+    private String tipoLivroPreferido1;
+    private String tipoLivroPreferido2;
+    private ArrayList<CargosUsuarios> cargosUsuarios = new ArrayList<CargosUsuarios>();
+    private ArrayList<Telefone> telefones = new ArrayList<Telefone>();
+    private ArrayList<Livro> livros = new ArrayList<Livro>();
+}
