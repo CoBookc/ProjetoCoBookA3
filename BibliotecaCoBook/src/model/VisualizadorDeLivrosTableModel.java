d586cc8ac4837014d0241231f8c459a570e3abf2
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-11-29 08:54:38 -0300

    Alterando classe para outro pacote e implementando um Override

--- BibliotecaCoBook/src/model/VisualizadorDeLivrosTableModel.java
+++ BibliotecaCoBook/src/model/VisualizadorDeLivrosTableModel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +26,7 @@ public class VisualizadorDeLivrosTableModel extends AbstractTableModel {
     private String[] colunas = {"titulo", "nota"};
     
     
+    @Override
     public String getColumnName (int column){
         return colunas[column];
     }
